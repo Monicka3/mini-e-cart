5ef48321ffdcf5982f5c8ed0d922a5149a0fd655
Monicka3 <monimska318@gmail.com>
2024-07-31 23:00:14 +0530

    e-cart

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Product from "./product.svelte";
+
+  let new_fruit = [
+    { name: "Apple", price: 160 },
+    { name: "Orange", price: 80 },
+    { name: "mango", price: 100 },
+  ];
+</script>
+
+{#each new_fruit as fruits}
+  <Product fruit={fruits.name} price={fruits.price} />
+{/each}
---
+++ src/product.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let fruit;
+  export let price
+  let quantity = 0;
+
+  function add() {
+    quantity = quantity + 1;
+  }
+  function minus() {
+    if(quantity>1)
+    quantity = quantity - 1;
+    else
+    quantity=0
+  }
+</script>
+{fruit}=${price}
+<div>
+  <button on:click={add}> +(add) </button>
+  {quantity}
+  <button on:click={minus}> -(minus) </button>
+</div>
